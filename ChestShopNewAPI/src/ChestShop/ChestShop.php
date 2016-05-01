c311fb8f298f99c5a9f9579287f57e805d2470dc
Skyladd <skyladd1@gmail.com>
2016-05-01 16:26:25 -0600

    Update ChestShop.php

--- ChestShopNewAPI/src/ChestShop/ChestShop.php
+++ ChestShopNewAPI/src/ChestShop/ChestShop.php
@@ -31,6 +31,7 @@ class ChestShop extends PluginBase
 
     public function onDisable()
     {
+        $this->getLogger()->info(TextFormat::GREEN . "Disabled");
     }
 
     public function onCommand(CommandSender $sender, Command $command, $label, array $args)
