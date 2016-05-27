869e728afd313343c34d165b88be0c32c1004334
Skyladd <skyladd1@gmail.com>
2016-05-27 17:31:33 -0600

    Update ChestShop.php
    Remove useless code

--- ChestShopNewAPI/src/ChestShop/ChestShop.php
+++ ChestShopNewAPI/src/ChestShop/ChestShop.php
@@ -14,7 +14,6 @@ class ChestShop extends PluginBase
 {
     public function onLoad()
     {
-        $this->getLogger()->info(TextFormat::GREEN . "Loading...");
     }
 
     public function onEnable()
@@ -26,6 +25,7 @@ class ChestShop extends PluginBase
     	}else{
     		$this->getLogger()->alert(TextFormat::RED . "Plugin disabled. Please use MassiveEconomy");
     		$this->getPluginLoader()->disablePlugin($this);
+    		
     	}
 
     }
@@ -33,26 +33,6 @@ class ChestShop extends PluginBase
     public function onDisable()
     {
         $this->getLogger()->info(TextFormat::RED . "Disabled");
-    }
-
-    public function onCommand(CommandSender $sender, Command $command, $label, array $args)
-    {
-        switch ($command->getName()) {
-            case "id":
-                $name = array_shift($args);
-                $constants = array_keys((new \ReflectionClass("pocketmine\\item\\Item"))->getConstants());
-                foreach ($constants as $constant) {
-                    if (stripos($constant, $name) !== false) {
-                        $id = constant("pocketmine\\item\\Item::$constant");
-                        $constant = str_replace("_", " ", $constant);
-                        $sender->sendMessage("ID:$id $constant");
-                    }
-                }
-                return true;
-
-            default:
-                return false;
-        }
     }
 }
 
