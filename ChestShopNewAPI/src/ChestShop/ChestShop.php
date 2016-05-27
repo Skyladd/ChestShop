debf183c3f214522ee65b1a9a6018a2d30b2e176
Skyladd <skyladd1@gmail.com>
2016-05-27 17:52:09 -0600

    Clean up code

--- ChestShopNewAPI/src/ChestShop/ChestShop.php
+++ ChestShopNewAPI/src/ChestShop/ChestShop.php
@@ -3,8 +3,6 @@
 namespace ChestShop;
 
 use pocketmine\plugin\PluginBase;
-use pocketmine\command\Command;
-use pocketmine\command\CommandSender;
 use pocketmine\utils\TextFormat;
 
 //MassiveEconomy API Call
@@ -12,28 +10,27 @@ use MassiveEconomy\MassiveEconomyAPI;
 
 class ChestShop extends PluginBase
 {
-    public function onLoad()
-    {
-    }
 
-    public function onEnable()
-    {
-        if (!file_exists($this->getDataFolder())) @mkdir($this->getDataFolder());
-        $this->getServer()->getPluginManager()->registerEvents(new EventListener($this, new DatabaseManager($this->getDataFolder() . 'ChestShop.sqlite3')), $this);
-        if(MassiveEconomyAPI::getInstance()->getAPIVersion() == "0.90"){ //Checking API version. Important for API Functions Calls
-    		$this->getLogger()->info(TextFormat::GREEN . "Enabled");
-    	}else{
-    		$this->getLogger()->alert(TextFormat::RED . "Plugin disabled. Please use MassiveEconomy");
-    		$this->getPluginLoader()->disablePlugin($this);
-    		
-    	}
+	public function onLoad()
+	{
+	}
 
-    }
+	public function onEnable()
+	{
+		if (!file_exists($this->getDataFolder())) @mkdir($this->getDataFolder());
+			$this->getServer()->getPluginManager()->registerEvents(new EventListener($this, new DatabaseManager($this->getDataFolder() . 'ChestShop.sqlite3')), $this);
+		if(MassiveEconomyAPI::getInstance()->getAPIVersion() == "0.90"){ //Checking API version. Important for API Function Calls
+			$this->getLogger()->info(TextFormat::GREEN . "Enabled");
+		}else{
+			$this->getLogger()->alert(TextFormat::RED . "Plugin disabled. Please use MassiveEconomy");
+			$this->getPluginLoader()->disablePlugin($this);
+		}
 
-    public function onDisable()
-    {
-        $this->getLogger()->info(TextFormat::RED . "Disabled");
-    }
+	}
+
+	public function onDisable()
+	{		
+	}
 }
 
 
